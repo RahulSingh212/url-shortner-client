6da05411d447c0eaa748f3c1b142c6dd36b567e4
RahulSingh212 <rahulsinghrs174326@gmail.com>
2024-08-16 18:13:21 +0530

    explained-the-functioning-of-the-context-api

--- src/context/PageContext.js
+++ src/context/PageContext.js
@@ -1,18 +1,18 @@
 import React, { createContext, useState } from 'react';
 
-// Create a Context
+// This context is used to store the details of url across multiple sections for the client.
+// 1. Stores the list of top 100 urls based upon the click count
+// 2. Stores the list of all the urls in the database
+// 3. Stores the list of all the urls based upon the search of the user
+// 4. Has the state functionality to close and open the sidebar in responsive ui
+
 const PageContext = createContext();
 
-// Create a Provider Component
 const PageProvider = ({ children }) => {
   const [top100LinkList, setTop100LinkList] = useState([]);
   const [allLinkList, setAllLinkList] = useState([]);
   const [searchList, setSearchList] = useState([]);
   const [sideBar, setSideBar] = useState(false);
-
-  const removeShortUrlHandler = async (dbId, isTopList = true) => {
-    
-  }
 
   const pageValue = {
     top100LinkList,
